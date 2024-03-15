94346582c533d9bc30536e967920ca47e3a004a9
adrien-brown <156132138+adrien-brown@users.noreply.github.com>
2024-03-15 10:46:30 +0000

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+"""This program print the words Git is awesome.
+Then it asks the user to write something"""
+
+print("Git is awesome!")
+user_input = input("Please write some text: ")
+
+
+print(f"You have written:  {user_input}")
